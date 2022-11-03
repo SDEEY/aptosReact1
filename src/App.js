015ab43be355e24eed9f584ac6f989b82574e963
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-11-03 20:53:24 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const aptAmount = '1.6'
-const image = 'https://media.discordapp.net/attachments/1033195024751542323/1036658480804794459/IMG_6736.png?width=610&height=610'
-const Title = "Wolfiez | Aptos"
-const supply = 3999
+const aptAmount = '6'
+const image = 'https://pbs.twimg.com/profile_images/1586247293002563585/r-QfRNbQ_400x400.jpg'
+const Title = "Aptos (MOVE)Rs NFT"
+const supply = 100
 
 document.title = Title
 document.getElementById('favicon').setAttribute('href', image)
