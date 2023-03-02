17747bf3f62d8d18a4fd2b99a1b398f47f47b85c
SDEEY <75689630+SDEEY@users.noreply.github.com>
2023-03-02 22:44:53 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import OwnLayout from "./OwnLayout/OwnLayout";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const aptAmount = '0.7'
-const image = 'https://media.discordapp.net/attachments/1071667467723755582/1075131054320529418/IMG_7316.png?width=624&height=609'
-const Title = "DEUS EX MACHINA"
+const aptAmount = '0.99'
+const image = 'https://pbs.twimg.com/profile_images/1624165192987811840/Q22GEC5T_400x400.jpg'
+const Title = "BEEP BOOP"
 const supply = 471
 
 document.title = Title
